d4fd1184f3ba2c5167474bf2d82e6a74cbaddc29
shweta sharma <shweta-sharma-1009@github.com>
2023-10-06 17:26:14 +0530

    added color

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
   const btn = document.getElementsByTagName('button')[0];
 const colors = [
-    'pink','green','purple','yellow','cyan'
+    'pink','green','purple','yellow','cyan','#e87fe0', '#3498db', '#e74c3c', '#1abc9c', '#f39c12', '#9b59b6', 
+    '#2ecc71', '#f1c40f', '#e67e22', '#95a5a6', '#34495e','#7f8c8d', '#d35400', '#c0392b', '#16a085', '#8e44ad',
+    '#7f8c8d', '#d35400', '#c0392b', '#16a085', '#8e44ad','#27ae60', '#d35400', '#2980b9', '#f39c12', '#bdc3c7','blue'
+    
 ];
 
 btn.addEventListener('click', function() {
